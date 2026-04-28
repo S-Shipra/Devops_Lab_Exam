693288d85f3a877707cc96e073e5b7bf7263efe5
S-Shipra <singh.shipra4005@gmail.com>
2026-04-28 15:48:07 +0530

    Revert "Added personalized greeting feature with user input"
    This reverts commit ddf36146a1c574343c4ea1d600ecef12286ad999.

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
 function changeText() {
-    document.getElementById("dynamicText").innerText =
-        "🎉 You clicked the button! This text has changed.";
+    document.getElementById("dynamicText").innerText = 
+        " You clicked the button! This text has changed.";
 }
-
 function toggleTheme() {
     document.body.classList.toggle("dark-mode");
-}
-
-function showGreeting() {
-    const name = document.getElementById("username").value;
-    const hour = new Date().getHours();
-    let message = "";
-
-    if (hour < 12) {
-        message = "Good Morning";
-    } else if (hour < 18) {
-        message = "Good Afternoon";
-    } else {
-        message = "Good Evening";
-    }
-
-    if (name) {
-        message += ", " + name + "!";
-    } else {
-        message += "!";
-    }
-
-    document.getElementById("greeting").innerText = message;
 }
